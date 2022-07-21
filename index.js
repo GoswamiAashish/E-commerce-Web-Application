68744761a0a3802bf4c644b12a8423917fc47d42
spandey6395 <spandey6395@gmail.com>
2022-07-21 14:50:56 +0530

    Ecommerce

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+
+
+dotenv.config()
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=>console.log("DB Connection successful"))
+.catch((err)=>{
+        console.log(err);
+    });
+
+
+app.use(express.json())                 // To read the data from req which is in form of json  
+app.use("/api/users" , userRoute);
+app.use("/api/auth" , authRoute);
+app.use("/api/products" , productRoute);
+app.use("/api/carts" , cartRoute);
+app.use("/api/orders" , orderRoute);
+
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log('Backend server is running');
+});
